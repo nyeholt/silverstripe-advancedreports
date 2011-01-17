304fdda35e84b8224214d86200c80d8161963fa9
Rodney Way <rodney@silverstripe.com.au>
2011-01-17 16:57:10 +1100

    test commit

--- _config.php
+++ _config.php
@@ -3,3 +3,5 @@
 if (($dirname = basename(dirname(__FILE__))) != 'advancedreports') {
 	exit("The Advanced Reports module MUST be placed in the advancedreports directory, not in $dirname");
 }
+
+//Temp comment - testing github
